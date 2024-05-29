4658168cd98d0c854bd9d009278e1354e7839dc0
rdeniele <rondenielep13@gmail.com>
2024-05-29 13:28:57 +0800

    first commit

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,95 @@
+// import React from 'react';
+import styles from './ProjectsStyles.module.css';
+import BeachwayImage from '../../assets/Beachway.png';
+import DwellImage from '../../assets/Dwell.png';
+import HTCImage from '../../assets/HTC.png';
+import HTLRImage from '../../assets/HTLR.png';
+import HTVRImage from '../../assets/HTVR.png';
+import KanokanoImage from '../../assets/Kanokano.png';
+import LBMSImage from '../../assets/LBMS.png';
+import MTSImage from '../../assets/MTS.png';
+import PBImage from '../../assets/PB.png';
+import SunriseImage from '../../assets/Sunrise.png';
+import ProjectCard from '../../common/ProjectCard';
+
+
+
+function Projects() {
+  return (
+    <section id='projects' className={styles.container}>
+        <h1 className="sectionTitle">Projects</h1>
+        <div className={styles.projectsContainer}>
+            <ProjectCard
+            src={BeachwayImage}
+            link="https://beachwayporta.com"
+            h3="Beachway Vacation Rentals"
+            p="Real Estate Website developed using Guesty"
+            />
+            <ProjectCard
+            src={DwellImage}
+            link="dwellluxuryrentals-8hm3.guestywebsites.com"
+            h3="Dwell Vacation Rentals"
+            p="Real Estate Website developed using Guesty"
+            />
+
+            <ProjectCard
+            src={HTCImage}
+            link="https://home-team-capital.com"
+            h3="Home Team Capital"
+            p="Real Estate Website developed using WordPress"
+            />
+
+            <ProjectCard
+            src={HTLRImage}
+            link="https://hometeamluxuryrealty.com"
+            h3="Home Team Luxury Realty"
+            p="Real Estate Website developed using WordPress"
+            />
+
+            <ProjectCard
+            src={HTVRImage}
+            link="https://hometeamvr.com"
+            h3="Home Team Luxury Realty"
+            p="Real Estate Website developed using Guesty"
+            />
+
+            <ProjectCard
+            src={KanokanoImage}
+            link="https://github.com/rdeniele/coffee_shop2"
+            h3="Kanokano Coffee Shop"
+            p="Coffee CRUD and POS system developed using Laravel"
+            />
+
+            <ProjectCard
+            src={LBMSImage}
+            link="https://github.com/rdeniele/coffee_shop2"
+            h3="Library Management System"
+            p="Library Management System with CRUD developed using native PHP"
+            />
+
+            <ProjectCard
+            src={MTSImage}
+            link="https://github.com/rdeniele/money_transfer"
+            h3="Money Transfer System"
+            p="Money Transfer System with CRUD developed using Laravel"
+            />
+
+            <ProjectCard
+            src={PBImage}
+            link="https://github.com/rdeniele/phonebok_laravel"
+            h3="Phonebook"
+            p="Phonebook with CRUD developed using Laravel"
+            />
+
+            <ProjectCard
+            src={SunriseImage}
+            link="'http://guesty.eu-responsivesiteeditor.com/preview/a29e8b4550dc406babc662d161e025aa"
+            h3="Sunrise Beach Vacation Rentals"
+            p="Sunrise Beach Vacation Rentals developed using Guesty"
+            />
+        </div>
+    </section>
+  )
+}
+
+export default Projects;
---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,70 @@
+import styles from './SkillsStyles.module.css';
+import checkMarkIconDark from '../../assets/checkmark-dark.svg';
+import checkMarkIconLight from '../../assets/checkmark-light.svg';
+import SkillList from '../../common/SkillList';
+import { useTheme } from '../../common/ThemeContext';
+
+function Skills() {
+  const { theme } = useTheme();
+  const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
+
+  return (
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="HTML" />
+        <SkillList src={checkMarkIcon} skill="CSS" />
+        <SkillList src={checkMarkIcon} skill="JavaScript" />
+        <SkillList src={checkMarkIcon} skill="TypeScript" />
+        <SkillList src={checkMarkIcon} skill="Node" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="Next" />
+        <SkillList src={checkMarkIcon} skill="Express" />
+        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Laravel" />
+        <SkillList src={checkMarkIcon} skill="Blade" />
+        <SkillList src={checkMarkIcon} skill="Bootstrap" />
+        <SkillList src={checkMarkIcon} skill="MySQL" />
+        <SkillList src={checkMarkIcon} skill="XAMPP" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="C#" />
+        <SkillList src={checkMarkIcon} skill="C++" />
+        <SkillList src={checkMarkIcon} skill="Java" />
+        <SkillList src={checkMarkIcon} skill="Photoshop" />
+        <SkillList src={checkMarkIcon} skill="Blender" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="WordPress" />
+        <SkillList src={checkMarkIcon} skill="Guesty" />
+        <SkillList src={checkMarkIcon} skill="Photoshop" />
+        <SkillList src={checkMarkIcon} skill="Canva" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Python" />
+        <SkillList src={checkMarkIcon} skill="Tensorflow" />
+        <SkillList src={checkMarkIcon} skill="PyTorch" />
+        <SkillList src={checkMarkIcon} skill="OpenCV" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="scikit-learn" />
+        <SkillList src={checkMarkIcon} skill="NLTK" />
+        <SkillList src={checkMarkIcon} skill="Django" />
+        <SkillList src={checkMarkIcon} skill="PostgresSQL" />
+        <SkillList src={checkMarkIcon} skill="Jupyter" />
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
